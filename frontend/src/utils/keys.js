fed58b9b1a8e258952058f4977360209a9c40388
AshotPoghosyan <ashot.poghosyan@easydmarc.us>
2025-02-10 02:05:18 +0400

    Define color variables for keys

--- frontend/src/utils/keys.js
+++ frontend/src/utils/keys.js
@@ -1,40 +1,46 @@
+const WHITE = "#FAFAFA"; // (250,250,250)
+const YELLOW = "#E6E6B4"; // (230,230,180)
+const RED = "#F0C8C8"; // (240,200,200)
+const VIOLET = "#DCC8DC"; // (220,200,220)
+const GREEN = "#C8F0C8"; // (200,240,200)
+const BLUE = "#C8D2FA"; // (200,210,250)
+
 export const KEYS = [
   [
-    { id: 1, english: "a", arabic: "\u064e", color: "#DCC8DC" }, // violet (220,200,220)
-    { id: 2, english: "b", arabic: "\u064f", color: "#DCC8DC" }, // violet
-    { id: 3, english: "c", arabic: "\u0650", color: "#DCC8DC" }, // violet
-    { id: 4, english: "d", arabic: "\u0642", color: "#C8D2FA" }, // blue (200,210,250)
-    { id: 5, english: "e", arabic: "\u062c", color: "#E6E6B4" }, // yellow (230,230,180)
-    { id: 6, english: "f", arabic: "\u0647", color: "#F0C8C8" }, // red (240,200,200)
-    { id: 7, english: "%", arabic: "\u066A", color: "#FAFAFA" }, // white (250,250,250)
+    { id: 1, english: "a", arabic: "\u064e", color: VIOLET },
+    { id: 2, english: "b", arabic: "\u064f", color: VIOLET },
+    { id: 3, english: "c", arabic: "\u0650", color: VIOLET },
+    { id: 4, english: "d", arabic: "\u0642", color: BLUE },
+    { id: 5, english: "e", arabic: "\u062c", color: YELLOW },
+    { id: 6, english: "f", arabic: "\u0647", color: RED },
+    { id: 7, english: "%", arabic: "\u066A", color: WHITE },
   ],
   [
-    { id: 8, english: "y", arabic: ">", color: "#FAFAFA" }, // white
-    { id: 9, english: "g", arabic: "\u0641", color: "#C8F0C8" }, // green (200,240,200)
-    { id: 10, english: "h", arabic: "\u0645", color: "#C8F0C8" }, // green
-    { id: 11, english: "i", arabic: "\u0643", color: "#C8D2FA" }, // blue
-    { id: 12, english: "j", arabic: "\u0633", color: "#F0C8C8" }, // red
-    { id: 13, english: "k", arabic: "\u062d", color: "#F0C8C8" }, // red
-    { id: 14, english: "l", arabic: "\u0639", color: "#F0C8C8" }, // red
+    { id: 8, english: "y", arabic: ">", color: WHITE },
+    { id: 9, english: "g", arabic: "\u0641", color: GREEN },
+    { id: 10, english: "h", arabic: "\u0645", color: GREEN },
+    { id: 11, english: "i", arabic: "\u0643", color: BLUE },
+    { id: 12, english: "j", arabic: "\u0633", color: RED },
+    { id: 13, english: "k", arabic: "\u062d", color: RED },
+    { id: 14, english: "l", arabic: "\u0639", color: RED },
   ],
   [
-    { id: 15, english: "m", arabic: "\u066D", color: "#E6E6B4" }, // yellow
-    { id: 16, english: "n", arabic: "\u066E", color: "#C8F0C8" }, // green
-    { id: 17, english: "o", arabic: "\u0646", color: "#F0C8C8" }, // red
-    { id: 18, english: "p", arabic: "\u0631", color: "#F0C8C8" }, // red
-    { id: 19, english: "q", arabic: "\u0635", color: "#F0C8C8" }, // red
-    { id: 20, english: "r", arabic: "\u0621", color: "#E6E6B4" }, // yellow
-    { id: 21, english: "z", arabic: "_", color: "#FAFAFA" }, // white
+    { id: 15, english: "m", arabic: "\u066D", color: YELLOW },
+    { id: 16, english: "n", arabic: "\u066E", color: GREEN },
+    { id: 17, english: "o", arabic: "\u0646", color: RED },
+    { id: 18, english: "p", arabic: "\u0631", color: RED },
+    { id: 19, english: "q", arabic: "\u0635", color: RED },
+    { id: 20, english: "r", arabic: "\u0621", color: YELLOW },
+    { id: 21, english: "z", arabic: "_", color: WHITE },
   ],
   [
-    { id: 22, english: "%", arabic: "_", color: "#FAFAFA" }, // white
-    { id: 23, english: "s", arabic: "\u0637", color: "#C8F0C8" }, // green
-    { id: 24, english: "t", arabic: "\u062F", color: "#C8D2FA" }, // blue
-    { id: 25, english: "u", arabic: "\u0644", color: "#DCC8DC" }, // violet
-    { id: 26, english: "v", arabic: "\u0627", color: "#E6E6B4" }, // yellow
-    { id: 27, english: "w", arabic: "\u0648", color: "#C8D2FA" }, // blue
-    { id: 28, english: "x", arabic: "\u064A", color: "#DCC8DC" }, // violet
-    { id: 29, english: "x", arabic: "\u064A", color: "#FAFAFA" }, // white
-
+    { id: 22, english: "%", arabic: "_", color: WHITE },
+    { id: 23, english: "s", arabic: "\u0637", color: GREEN },
+    { id: 24, english: "t", arabic: "\u062F", color: BLUE },
+    { id: 25, english: "u", arabic: "\u0644", color: VIOLET },
+    { id: 26, english: "v", arabic: "\u0627", color: YELLOW },
+    { id: 27, english: "w", arabic: "\u0648", color: BLUE },
+    { id: 28, english: "x", arabic: "\u064A", color: VIOLET },
+    { id: 29, english: "x", arabic: "\u064A", color: WHITE },
   ],
 ];
