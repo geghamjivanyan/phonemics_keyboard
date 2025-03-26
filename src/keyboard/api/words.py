22cd9411c9dc8b06c0d3e93db64e4d694bfafeda
gegham <gegham>
2025-03-26 21:48:46 +0400

    minor

--- src/keyboard/api/words.py
+++ src/keyboard/api/words.py
@@ -14,7 +14,19 @@ from django.db.models import Q
 
 from ..models.words import Word
 from ..models.koran import Koran
+from ..models.translit_words import TranslitWord
 
+#
+def is_vowel(letter):
+    """
+    check if letter is vowel
+
+    :params letter - letter which should be checked
+
+    :returns: True or False
+    """
+    vowels = 'aiuAYNW*'
+    return letter in vowels
 
 class WordView(View):
 
@@ -187,6 +199,16 @@ class WordView(View):
                     result = Word.objects.filter(prev=words[1])
                     for r in result:
                         data.append(r.current)
+        else:
+            print("TRANSLIT")
+            arabic = WordView._from_arabic_to_translit(words)
+            print("ARABIC", arabic)
+            cut = WordView.split(arabic)
+            print("CUT", cut)
+
+            translits = cut.split(' ')
+            print("TRANSLIT", translits)
+            data = WordView.suggest(translits)
 
         data = {
             "rhythms": [],
@@ -200,9 +222,9 @@ class WordView(View):
         )
 
     @staticmethod
-    def _from_arabic_to_translit(text):
+    def _from_translit_to_arabic(text):
 
-        translate = {
+        translit = {
             'aa': chr(0x064E) + chr(0x0627),
             'AA': chr(0x064E) + chr(0x0627),
             'ii': chr(0x0650) + chr(0x064A),
@@ -234,9 +256,114 @@ class WordView(View):
             'q':  chr(0x0642),
             'k':  chr(0x0643),
             'l':  chr(0x0644),
+            'L':  chr(0x0644),
             'm':  chr(0x0645),
             'n':  chr(0x0646),
             'h':  chr(0x0647),
             'w':  chr(0x0648),
             'y':  chr(0x064A),
         }
+
+        s = ''
+        i = 0
+        while i < len(text):
+
+            if text[i:i+2] in translit:
+                s += translit[text[i:i+2]]
+                i += 2
+            else:
+                print("TEXT I", text[i])
+                s += translit[text[i]]
+                i += 1
+
+        return s 
+    
+    @staticmethod
+    def _from_arabic_to_translit(text):
+
+        translit = {
+            chr(0x064E) + chr(0x0627): 'aa',
+            chr(0x064E) + chr(0x0627): 'AA',
+            chr(0x0650) + chr(0x064A): 'ii',
+            chr(0x064F) + chr(0x0648): 'uu',
+            chr(0x064E): 'A',
+            chr(0x064E): 'a',
+            chr(0x0650): 'i',
+            chr(0x064F): 'u',
+            chr(0x0621): 'ʼ',
+            chr(0x0639): 'ʻ',
+            chr(0x0628): 'b',
+            chr(0x062A): 't',
+            chr(0x062F): 'd',
+            chr(0x062C): 'ǧ',
+            chr(0x0631): 'r',
+            chr(0x062B): 'ṯ',
+            chr(0x0632): 'z',
+            chr(0x0633): 's',
+            chr(0x062D): 'ḥ',
+            chr(0x062E): 'ḫ',
+            chr(0x0630): 'ḏ',
+            chr(0x0635): 'ṣ',
+            chr(0x0634): 'š',
+            chr(0x0636): 'ḍ',
+            chr(0x0638): 'ẓ',
+            chr(0x0637): 'ṭ',
+            chr(0x063A): 'ġ',
+            chr(0x0641): 'f',
+            chr(0x0642): 'q',
+            chr(0x0643): 'k',
+            chr(0x0644): 'l',
+            chr(0x0644): 'L',
+            chr(0x0645): 'm',
+            chr(0x0646): 'n',
+            chr(0x0647): 'h',
+            chr(0x0648): 'w',
+            chr(0x064A): 'y',
+        }
+
+        s = ''
+        i = 0
+        while i < len(text):
+
+            if text[i:i+2] in translit:
+                s += translit[text[i:i+2]]
+                i += 2
+            else:
+                s += translit[text[i]]
+                i += 1
+
+        return s 
+    
+    @staticmethod
+    def split(res):
+        s = ''
+        i = 0
+        while i < len(res)-1:
+            if not is_vowel(res[i]) and is_vowel(res[i+1]):
+                s += ' ' + res[i:i+2]
+                i += 2
+            else:
+                s += res[i]
+                i += 1
+        s += res[-1]
+        
+        return s.strip()
+    
+    @staticmethod
+    def suggest(cut):
+
+        data = []
+        if len(cut) == 1:
+            suggestions = TranslitWord.objects.filter(prev=cut[0])
+            for suggest in suggestions:
+                if suggest.current:
+                    res = WordView._from_translit_to_arabic(suggest.current)
+                    data.append(res)
+        if len(cut) > 1:
+            suggestions = TranslitWord.objects.filter(prev=cut[-2], current=cut[-1])
+            for suggest in suggestions:
+                if suggest.next:
+                    res = WordView._from_translit_to_arabic(suggest.next)
+                    data.append(res)
+
+        return data
