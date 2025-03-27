04040f5dd7cf24f8caf2a6b3fd98c39df768e97c
AshotPoghosyan <ashot.poghosyan@easydmarc.us>
2025-03-27 20:09:58 +0400

    Make old folder and move there unused transformations and constants

--- frontend/src/util/index.js
+++ frontend/src/util/index.js
@@ -2,12 +2,6 @@ export {
   ArabicPhonemicTransformer,
   ARABIC_ORTHOGRAPHIC_RULES,
 } from "./transformation/transformation";
-export {
-  applyTransformations,
-  NEW_TRANSFORMATIONS,
-} from "./transformation/new-transformation";
 export { DOT_TRANSFORMATIONS } from "./transformation/dot-transformation";
 export { escapeRegex } from "./escape-regex";
 export { KEYBOARD_1, KEYBOARD_2 } from "./keyboards";
-export { patternRecommendations } from "./patternRecommendations";
-export { SPACE_TRANSFORMATIONS } from "./transformation/space-transformation";
