b507c3f168423695a83c4b68a68d6a4578d18445
gegham <gegham>
2025-05-04 18:51:27 +0400

    db settings

--- src/keyboard/settings.py
+++ src/keyboard/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'keyboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -113,6 +113,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
